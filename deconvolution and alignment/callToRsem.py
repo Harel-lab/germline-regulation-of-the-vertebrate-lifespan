38319062ebe97a426497ab60bc2a942fd2fe549d
Itamar Harel <66352189+Harel-lab@users.noreply.github.com>
2022-12-01 18:32:07 +0200

    Update callToRsem.py

--- deconvolution and alignment/callToRsem.py
+++ deconvolution and alignment/callToRsem.py
@@ -4,41 +4,21 @@
 import os, sys
 import re, time
 
-""" Henrik- senescence
-path = "/cs/icore/tehila_atlan/data/Henrik/"
-batch = ["Henrik_SC_E1_", "Henrik_SC_E2_", "Henrik_SC_E3_", "Henrik_SC_C1_", "Henrik_SC_C2_", "Henrik_SC_C3_"]#,  # ["pertRANDOMminus2i_"]  # ["Chen_ES_"]
 
-# Eitan germ
-path = '/sci/labs/itamarh/tehila_atlan/icore-home/data/scRNAseq/Eitan/'
+path = 'icore-home/data/scRNAseq/'
+""" 
+# scRNAseq germ
 batch = ['KilliWT_M1_', 'KilliKO_F1_', 'KilliKO_M3_', 'KilliWT_F2_']
 
-# Eitan germ2
-path = '/sci/labs/itamarh/tehila_atlan/icore-home/data/scRNAseq/Eitan/'
+# scRNAseq germ2
 batch = ['WT_F2_', 'KO_M3_', 'WT_M3_', 'KO_F1_']
 
-# Eitan germ3
-path = '/sci/labs/itamarh/tehila_atlan/icore-home/data/scRNAseq/Eitan/'
+# scRNAseq germ3
 batch = ['KOF3_', 'KOF4_', 'KOM1_', 'KOM2_', 'WTF1_', 'WTF3_', 'WTM2_']
 """
 
-# Eitan germ4
-path = '/sci/labs/itamarh/tehila_atlan/icore-home/data/scRNAseq/Eitan/'
-batch = ['KfWTM1_']
-#'KfWTF1_', 'KfWTF3_', 'KfKOF2_', 'KfKOF3_', 'KfWTM2_', 'KfKOM2_', 'KfKOM4_', 'KfWTM1_'
-
-
-
-
-
-
-# path = "/cs/icore/shirliyadadon/data/ido_amit/"
-# batch = ["SRR507"]
-# files = os.popen('ls ' + path + batch[0] + "*.fastq").read().split('\n')[:-1]  # take names of all fastq files of this experiment
-# files = [f.split('/')[-1].split('.')[0] for f in files]  # create a list of all the names without the last-empty string
-# randFiles = files[0:len(files):2]asxa
-# randFiles = ['Rand_'+f for f in randFiles]
-# path = sys.argv[1]
-# batch = sys.argv[2:]
+# scRNAseq germ4
+batch = ['KfWTM1_','KfWTF1_', 'KfWTF3_', 'KfKOF2_', 'KfKOF3_', 'KfWTM2_', 'KfKOM2_', 'KfKOM4_', 'KfWTM1_']
 
 for b in batch:
     # newPath = path + "SC_" + b[:-1]
@@ -59,14 +39,9 @@ for b in batch:
             outRsemMus = dirRsem + "/" + filePath.group(1) + ".mm9"
             outRsemH = dirRsem + "/" + filePath.group(1) + ".hg38"
             outRsemK = dirRsem + "/" + filePath.group(1) + ".kf"
-            # print("starting: " + filePath.group(1)+"\n")
-            # print("sbatch --mem=150g --time=1-0 --job-name=RSEM" + filePath.group(1)[:2] +
-            #     " -o /sci/labs/itamarh/tehila_atlan/icore-home/logs/RSEMLogIdo.log --wrap=\"python3 /sci/labs/itamarh/tehila_atlan/icore-home/shirliyaScripts/rsem.py "
-            #     + fastq + " " + outRsemMus + " " + outRsemH + " " + outRsemK + " " + orgMus + " " + orgH + " " + orgK + "\"")
-            print(fastq)
             os.popen(
                 "sbatch --mem=150g --time=1-0 --job-name=RSEM" + filePath.group(1)[:2] +
-                " -o /sci/labs/itamarh/tehila_atlan/icore-home/logs/RSEMLogIdo.log --wrap=\"python3 /sci/labs/itamarh/tehila_atlan/icore-home/shirliyaScripts/rsem.py "
+                " -o icore-home/logs/RSEMLogIdo.log --wrap=\"python3 rsem.py "
                 + fastq + " " + outRsemMus + " " + outRsemH + " " + outRsemK + " " + orgMus + " " + orgH + " " + orgK + "\"")
             count += 1
             if count == 505:
