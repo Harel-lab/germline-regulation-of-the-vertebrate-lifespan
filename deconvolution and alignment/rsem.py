6a907e1d0a1ed5966a5db12a91fde3bb1d2dd815
Itamar Harel <66352189+Harel-lab@users.noreply.github.com>
2022-12-01 18:35:35 +0200

    Update rsem.py

--- deconvolution and alignment/rsem.py
+++ deconvolution and alignment/rsem.py
@@ -11,30 +11,15 @@ orgMus = sys.argv[5]
 orgH = sys.argv[6]
 orgK = sys.argv[7]
 
-org = "killifish" #"mouse" #"human"  #
+org = "killifish" 
 
-if org == "mouse":
-    os.popen(
-        "/cs/cbio/tommy/Software/RSEM/rsem-calculate-expression --num-threads 1 --bowtie2"
-        " --single-cell-prior --estimate-rspd " + fastq + " /cs/icore/tehila_atlan/RefGenomes/"
-        + orgMus + "/RSEM/RefSeq_RSEM " + outRsemMus)
-elif org == "human":
-    os.popen(
-        "/cs/cbio/tommy/Software/RSEM/rsem-calculate-expression --num-threads 1 --bowtie2"
-        " --single-cell-prior --estimate-rspd " + fastq + " /cs/icore/tehila_atlan/RefGenomes/"
-        + orgH + "/RefSeq_RSEM " + outRsemH)
-elif org == "killifish":
+
+if org == "killifish":
     rsem_line = "rsem-calculate-expression --num-threads 1 --bowtie2 --single-cell-prior --estimate-rspd " \
-                + fastq + " /sci/labs/itamarh/tehila_atlan/icore-home/RefGenomes/" + orgK + "/RefSeq_RSEM " + outRsemK
+                + fastq + " icore-home/RefGenomes/" + orgK + "/RefSeq_RSEM " + outRsemK
 
 
-    #   /sci/labs/itamarh/tehila_atlan/icore-home/Programs/RSEM/RSEM-1.3.3/
-    # rsem_line = 'rsem-calculate-expression --num-threads 1 --bowtie2 --single-cell-prior --estimate-rspd /sci/labs/itamarh/tehila_atlan/icore-home/data/scRNAseq/Eitan/KO_F1/ACGCTTTAGATGGGT.fastq /sci/labs/itamarh/tehila_atlan/icore-home/RefGenomes/Nfu_20140520/RefSeq_RSEM /sci/labs/itamarh/tehila_atlan/icore-home/data/scRNAseq/Eitan/KO_F1/rsem_output/ACGCTTTAGATGGGT.kf'
     print(rsem_line)
     log = os.popen(rsem_line).read()
     os.popen(rsem_line)
-    # print(log)
 print("End program")
-
-
-#/cs/cbio/tommy/Software/RSEM/
